8772aea45a803d05e0808ffd83c7e45f8f81b3d7
Ondrej Certik <ondrej@certik.cz>
2011-02-22 13:00:58 -0800

    Fix the config

--- _static/config/MathJax.js
+++ _static/config/MathJax.js
@@ -1,7 +1,9 @@
-extensions: ["jsMath2jax.js"],
-jax: ["input/TeX", "output/HTML-CSS"],
-tex2jax: {
-    inlineMath: [ ['$','$'], ["\\(","\\)"] ],
-    displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
-},
-"HTML-CSS": { availableFonts: ["TeX"] }
+MathJax.Hub.Config({
+    extensions: ["jsMath2jax.js"],
+    jax: ["input/TeX", "output/HTML-CSS"],
+    tex2jax: {
+        inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+        displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+    },
+    "HTML-CSS": { availableFonts: ["TeX"] }
+});
