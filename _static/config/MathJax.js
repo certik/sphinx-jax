fff560201a98bffb8059dda1cd6c965b0d5c6f60
Ondrej Certik <ondrej@certik.cz>
2011-02-22 12:57:14 -0800

    Add a config file

---
+++ _static/config/MathJax.js
@@ -0,0 +1,7 @@
+extensions: ["jsMath2jax.js"],
+jax: ["input/TeX", "output/HTML-CSS"],
+tex2jax: {
+    inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+    displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+},
+"HTML-CSS": { availableFonts: ["TeX"] }
